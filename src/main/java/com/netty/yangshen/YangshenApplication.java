5adf048c558b1ec9fc6f4b4a448e17f29b7ecff5
yang.shen <yang.shen@zenlayer.com>
2020-08-20 17:32:27 +0800

    netty-study

---
+++ src/main/java/com/netty/yangshen/YangshenApplication.java
@@ -0,0 +1,13 @@
+package com.netty.yangshen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangshenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YangshenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/netty/yangshen/YangshenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netty.yangshen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YangshenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
